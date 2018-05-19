fba5ddda0833bf4d35f39257ec4a928ca6f3d18a
dpszczolek <35000772+dpszczolek@users.noreply.github.com>
2018-05-19 21:59:49 +0200

    "24.2"

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
-
-  var apiRoot = 'https://shielded-chamber-92843.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://shielded-chamber-92843.herokuapp.com/v1/trello/';
+  const apiRoot = 'http://localhost:8880/v1/task/';
+  const trelloApiRoot = 'http://localhost:8880/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
