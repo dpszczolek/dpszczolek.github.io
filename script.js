64c17465bd617886e4dcd41b2f4c6550bf296a25
dpszczolek <35000772+dpszczolek@users.noreply.github.com>
2018-05-22 00:09:16 +0200

    24.4

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://localhost:8880/v1/';
+  const apiRoot = 'http://localhost:8880/v1/';
   const trelloApiRoot = 'http://localhost:8880/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
